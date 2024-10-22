bfcefcc290998e35a0f039b7813799131b108166
seihareika <133826539+seihareika@users.noreply.github.com>
2024-10-22 15:21:38 +0900

    Add files via upload

---
+++ 20js/top_header_depth.js
@@ -0,0 +1,24 @@
+document.write(`
+    <header>
+    <h1></h1><h2></h2>
+    <div class="openbtn"><span></span><span></span><span></span></div>
+    <nav id="g-nav">
+        <div id="g-nav-list">
+            <ul>
+                <li><a href="../index.html"><h1>TOP</h1></a></li>
+                <br><hr><br>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+            </ul>
+        </div>
+    </nav>
+    </header>
+    `);
