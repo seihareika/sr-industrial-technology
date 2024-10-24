71f82bda6a32d5bdc42ecd5331649a9d91acb163
seihareika <133826539+seihareika@users.noreply.github.com>
2024-10-24 15:45:25 +0900

    Add files via upload

--- 20js/python_header_depth.js
+++ 20js/python_header_depth.js
@@ -2,12 +2,13 @@ document.write(`
     <header>
     <div class="header_back">
         <a href="./index.html">
-            <img class="header_image" src="./30image/10icon/gear.png" alt="Gear">
+            <img class="header_image" src="../30image/10icon/gear.png" alt="Gear">
         </a>
         <h1 class="header_h1">header text1</h1><!--<br>--><h2 class="header_h2">header text2</h2>
     </div>
     <hr>
     <div class="openbtn"><span></span><span></span><span></span></div>
+
     <nav id="g-nav">
         <div id="g-nav-list">
             <ul>
@@ -17,6 +18,7 @@ document.write(`
             </ul>
         </div>
     </nav>
+
     </header>
-    <div class="home_btn"><a class="home_btn_a" href="./index.html#01">ホームへ戻る</a></div>
-`);
+    
+    `);
