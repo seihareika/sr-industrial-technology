b5ca771478fc9c83752b8200e499996e39eb4b29
seihareika <133826539+seihareika@users.noreply.github.com>
2024-10-25 10:37:27 +0900

    Add files via upload

---
+++ 20js/we_header_depth.js
@@ -0,0 +1,22 @@
+document.write(`
+    <header>
+    <div class="header_back">
+        <a href="./index.html">
+            <img class="header_image" src="../../../../30image/10icon/gear.png" alt="Gear">
+        </a>
+        <h1 class="header_h1">header text1</h1><!--<br>--><h2 class="header_h2">header text2</h2>
+    </div>
+    <hr>
+    <div class="openbtn"><span></span><span></span><span></span></div>
+    <nav id="g-nav">
+        <div id="g-nav-list">
+            <ul>
+                <a href="../../../../index.html"><li><p class="menu_p1">Top page</p></li></a>
+                <br><hr><br>
+                <a href="../../../../notfound.html"><li><p class="menu_p2">Sub page</p></li></a>
+            </ul>
+        </div>
+    </nav>
+    </header>
+    <div class="home_btn"><a class="home_btn_a" href="../../../../index.html#01">ホームへ戻る</a></div>
+`);
