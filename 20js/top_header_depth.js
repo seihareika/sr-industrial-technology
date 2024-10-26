29499b4e66f4db646a7c1037ad398a485f7c6721
seihareika <133826539+seihareika@users.noreply.github.com>
2024-10-26 13:58:34 +0900

    Add files via upload

--- 20js/top_header_depth.js
+++ 20js/top_header_depth.js
@@ -1,22 +1,26 @@
 document.write(`
     <header>
-    <div class="header_back">
-        <a href="./index.html">
-            <img class="header_image" src="./30image/10icon/gear.png" alt="Gear">
-        </a>
-        <h1 class="header_h1">header text1</h1><!--<br>--><h2 class="header_h2">header text2</h2>
-    </div>
-    <hr>
-    <div class="openbtn"><span></span><span></span><span></span></div>
-    <nav id="g-nav">
-        <div id="g-nav-list">
-            <ul>
-                <a href="../index.html"><li><p class="p1">Top page</p></li></a>
-                <br><hr><br>
-                <a href=""><li><p class="p2">Sub page</p></li></a>
-            </ul>
+        <div class="header_back">
+            <a href="./index.html">
+                <img class="header_image" src="./30image/10icon/gear.png" alt="Gear">
+            </a>
+            <h1 class="header_h1">header text1</h1><!--<br>--><h2 class="header_h2">header text2</h2>
         </div>
-    </nav>
+        <hr>
+            <div class="openbtn_top" id="btn19">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+                <nav id="g-nav">
+                    <div id="g-nav-list">
+                        <ul>
+                            <a href="../index.html"><li><p class="p1">Top page</p></li></a>
+                            <br><hr><br>
+                            <a href=""><li><p class="p2">Sub page</p></li></a>
+                        </ul>
+                    </div>
+                </nav>
     </header>
-    <div class="home_btn"><a class="home_btn_a" href="./index.html#01">ホームへ戻る</a></div>
+        <div class="home_btn"><a class="home_btn_a" href="./index.html#01">ホームへ戻る</a></div>
 `);
--- 20js/we_header_depth.js
+++ 20js/we_header_depth.js
@@ -1,22 +1,27 @@
 document.write(`
     <header>
-    <div class="header_back">
-        <a href="./index.html">
-            <img class="header_image" src="../../../../30image/10icon/gear.png" alt="Gear">
-        </a>
-        <h1 class="header_h1">SR Industrial Technology</h1><!--<br>--><h2 class="header_h2">Weather & Earthquakes</h2>
-    </div>
-    <hr>
-    <div class="openbtn"><span></span><span></span><span></span></div>
-    <nav id="g-nav">
-        <div id="g-nav-list">
-            <ul>
-                <a href="../../../../index.html"><li><p class="menu_p1">Top page</p></li></a>
-                <br><hr><br>
-                <a href="../../../../notfound.html"><li><p class="menu_p2">Sub page</p></li></a>
-            </ul>
+        <div class="header_back">
+            <a href="./index.html">
+                <img class="header_image" src="../../../../30image/10icon/gear.png" alt="Gear">
+            </a>
+            <h1 class="header_h1">SR Industrial Technology</h1><!--<br>--><h2 class="header_h2">Weather & Earthquakes</h2>
         </div>
-    </nav>
+        <hr>
+        <div class="openbtn_top" id="btn19">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <nav id="g-nav">
+            <div id="g-nav-list">
+                <ul>
+                    <a href="../../../../index.html"><li><p class="menu_p1">Top page</p></li></a>
+                    <br><hr><br>
+                    <a href="../../../../notfound.html"><li><p class="menu_p2">Sub page</p></li></a>
+                </ul>
+            </div>
+        </nav>
     </header>
-    <div class="home_btn"><a class="home_btn_a" href="../../../../index.html#01">ホームへ戻る</a></div>
+    <div class="home_btn"><a class="home_btn_a" href="../../../../index.html#01">ホーム</a></div>
+    <div class="home_btn2"><a class="home_btn_a2" href="#001">トップ</a></div>
 `);
