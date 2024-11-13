b3c9fae3b63bc6890f785eb94c547465cdb7698a
seihareika <133826539+seihareika@users.noreply.github.com>
2024-11-13 09:06:28 +0900

    Add files via upload

---
+++ 50contents/test/gacha_input.php
@@ -0,0 +1,13 @@
+<?php 
+    print("
+        <form action=\"./gacha.php\" method=\"post\">
+            1回
+            <input type=\"text\" name=\"usestone\" id=\"\">個消費で<br><br>
+            確率
+            <input type=\"text\" name=\"kakuritu\" id=\"\">%の<br><br>
+            <input type=\"number\" name=\"ren\" id=\"\">連を<br><br>
+            <input type=\"number\" name=\"kai\" id=\"\">回<br><br>回す<br><br>
+            <input type=\"submit\" value=\"send\">
+        </form>
+    ");
+?> 
